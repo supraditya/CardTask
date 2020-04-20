46d6cd4bb1a2f32875fc61bcd3a1ebe14c52fb38
[supraditya] <[aditya300100@gmail.com]>
2020-04-20 23:37:29 +0530

    Added styling, used Jquery to load JSON from API.

--- script.js
+++ script.js
@@ -1,3 +1,22 @@
-var i=0;
-i++;
-console.log(i);
+
+function loadAPI()
+{
+    // $("#result").load("https://picsum.photos/id/0/info");
+    // alert("Page has been loaded");
+    var url = "https://picsum.photos/id/0/info";
+    $.ajax
+    ({
+        url: url,
+        type: 'GET',
+        dataType: 'json',
+        success: function(data)
+        {
+            var result = data;
+            console.log(result);
+            // JSON object fetched in var data
+            var title1=document.getElementById('title1');
+            title1.innerHTML=result.author;
+        }
+    });
+    
+}
