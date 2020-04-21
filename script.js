e3f4cec60d4789ac4703eebc5638e972b2c80d35
[supraditya] <[aditya300100@gmail.com]>
2020-04-21 15:09:23 +0530

    Styling changes. Added image dimension info to card

--- script.js
+++ script.js
@@ -15,9 +15,35 @@ function loadAPI()
             console.log(result);
             // JSON object fetched in var data
             var title1=document.getElementById('title1');
-            title1.innerHTML=result.author;
-            var url=result.url;
-            console.log(url);
+            var title2=document.getElementById('title2');
+            var title3=document.getElementById('title3');
+            var title4=document.getElementById('title4');
+            title1.innerHTML="By: "+result.author;
+            title2.innerHTML="By: "+result.author;
+            title3.innerHTML="By: "+result.author;
+            title4.innerHTML="By: "+result.author;
+            var dim1=document.getElementById('dim1');
+            var dim2=document.getElementById('dim2');
+            var dim3=document.getElementById('dim3');
+            var dim4=document.getElementById('dim4');
+            dim1.innerHTML=result.height+" x "+result.width;
+            dim2.innerHTML=result.height+" x "+result.width;
+            dim3.innerHTML=result.height+" x "+result.width;
+            dim4.innerHTML=result.height+" x "+result.width;
+            var url1=result.url;
+            console.log(url1);
+            var url2=result.download_url;
+            $(".download").attr("href",url2);
+            // $(".card-img-top").attr("src",url1);
+             $("a").click(
+                 function(e) {   
+                     e.preventDefault();           
+                     //open download link in new page
+                     window.open( $(this).attr("href") );
+                 }
+             );
+             $("#img1").attr("src",url1);
+
         }
     });
     
